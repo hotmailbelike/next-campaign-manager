29671e999ac41dee445bec1573fdbbd22c71558e
Abdullah Omar <mdabdullahomar97@gmail.com>
2024-12-30 05:49:26 +0600

    fix: add sidebar toggle button on mobile layout

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,6 @@
-import { Button } from '@/components/ui/button';
+'use client';
+
+import { useSidebar } from '@/components/ui/sidebar';
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -8,11 +10,32 @@ import {
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Menu } from 'lucide-react';
+import { useEffect } from 'react';
 
 export function Navbar() {
+	const { setOpen, setOpenMobile, isMobile } = useSidebar();
+
+	useEffect(() => {
+		if (!isMobile) {
+			setOpen(true);
+		}
+	}, [isMobile]);
+
 	return (
 		<div className='flex items-center justify-between border-b bg-white px-6 py-7'>
-			<h1 className='text-xl text-gray-900 font-semibold'>Campaign</h1>
+			<h1 className='text-xl text-gray-900 font-semibold flex items-center gap-2	'>
+				{isMobile && (
+					<Menu
+						className='hover:cursor-pointer'
+						onClick={() => {
+							setOpenMobile(true);
+							console.log('first');
+						}}
+					/>
+				)}
+				Campaign
+			</h1>
 			<div className='flex items-center gap-4'>
 				<Avatar className='hover:cursor-pointer w-6 h-6'>
 					<AvatarImage src='headphones-icon.svg' />
