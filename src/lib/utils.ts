15ca72458c2b99b4b79f609dbfe31565d89152a5
Abdullah Omar <mdabdullahomar97@gmail.com>
2024-12-30 05:50:56 +0600

    chore: util function to extract month and year

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,32 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function getMonthYear(date: Date): string {
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+
+	const monthName = months[date.getMonth()];
+	const year = date.getFullYear();
+
+	return `${monthName} ${year}`;
+}
+
+export function capitalizeFirstLetter(str: string): string {
+	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
