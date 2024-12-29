3d4efa1a6d4ddce368d2e7776e31d8d2adf6010a
Abdullah Omar <mdabdullahomar97@gmail.com>
2024-12-30 05:50:05 +0600

    feat: api to list distinct campaign names

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -8,3 +8,8 @@ export const listCampaigns = async (): Promise<PaginatedCampaignResponse> => {
 	const response = await fetch(`${serverUrl}/campaigns`);
 	return response.json();
 };
+
+export const distinctCampaignNames = async (): Promise<string[]> => {
+	const response = await fetch(`${serverUrl}/campaigns/names/distinct`);
+	return response.json();
+};
