c71d643494e16800d03126119348de81ec683647
Abdullah Omar <mdabdullahomar97@gmail.com>
2024-12-30 18:36:46 +0600

    feat: create campaign function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,15 +1,62 @@
 'use server';
 
-import { PaginatedCampaignResponse } from '@/lib/types';
+import {
+	PaginatedCampaignResponse,
+	Campaign,
+	CreateCampaign,
+	ErrorResponse,
+} from '@/lib/types';
 
 const serverUrl = process.env.SERVER_URL;
 
-export const listCampaigns = async (): Promise<PaginatedCampaignResponse> => {
-	const response = await fetch(`${serverUrl}/campaigns`);
-	return response.json();
+export const listCampaigns = async (): Promise<
+	PaginatedCampaignResponse | ErrorResponse
+> => {
+	try {
+		const response = await fetch(`${serverUrl}/campaigns`);
+		return response.json();
+	} catch (error) {
+		console.error('📣 -> file: index.ts:11 -> listCampaigns -> error:', error);
+		if (error instanceof Error) {
+			return { message: error.message, code: 500 };
+		}
+
+		return { message: 'Unknown error occurred', code: 500 };
+	}
 };
 
-export const distinctCampaignNames = async (): Promise<string[]> => {
-	const response = await fetch(`${serverUrl}/campaigns/names/distinct`);
-	return response.json();
+export const distinctCampaignNames = async (): Promise<string[] | ErrorResponse> => {
+	try {
+		const response = await fetch(`${serverUrl}/campaigns/names/distinct`);
+		return response.json();
+	} catch (error) {
+		console.error('📣 -> file: index.ts:33 -> distinctCampaignNames -> error:', error);
+		if (error instanceof Error) {
+			return { message: error.message, code: 500 };
+		}
+
+		return { message: 'Unknown error occurred', code: 500 };
+	}
+};
+
+export const createCampaign = async (
+	data: CreateCampaign
+): Promise<Campaign | ErrorResponse> => {
+	try {
+		const response = await fetch(`${serverUrl}/campaigns`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+		return response.json();
+	} catch (error) {
+		console.error('📣 -> file: index.ts:56 -> error:', error);
+		if (error instanceof Error) {
+			return { message: error.message, code: 500 };
+		}
+
+		return { message: 'Unknown error occurred', code: 500 };
+	}
 };
