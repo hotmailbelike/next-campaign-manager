c6ace21d5bc673fbbbe4439822626972835f1547
Abdullah Omar <mdabdullahomar97@gmail.com>
2024-12-30 21:33:11 +0600

    chore: lodash debounce and truthy obj checker

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -30,3 +30,7 @@ export function getMonthYear(date: Date): string {
 export function capitalizeFirstLetter(str: string): string {
 	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export function hasTruthyValue(obj: Record<string, any>): boolean {
+	return Object.values(obj).some((value) => Boolean(value));
+}
